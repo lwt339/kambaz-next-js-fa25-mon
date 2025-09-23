449ae2224ab2d36c92a0c75e2bf9eb305f14374e
lwt339 <vickyliu.1223@gmail.com>
2025-09-22 22:07:37 -0400

    Assignment 2: Complete CSS and Bootstrap implementation

--- app/Labs/Lab2/BootstrapNavigation.tsx
+++ app/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,54 +1,54 @@
 import { Nav, NavItem, NavLink, Card, CardImg, CardBody, CardTitle, CardText, Button } from "react-bootstrap";
 
 export default function BootstrapNavigation() {
-  return (
-    <>
-      {/* NAV 1: Tabs - REQUIRED */}
-      <div id="wd-css-navigating-with-tabs">
-        <h2>Tabs</h2>
-        <Nav variant="tabs">
-          <NavItem>
-            <NavLink href="/Labs/Lab2/Active">Active</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/Labs/Lab2/Link1">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/Labs/Lab2/Link2" disabled>Disabled</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
-      
-      {/* NAV 2: Pills - REQUIRED */}
-      <div id="wd-css-navigating-with-pills" className="mt-3">
-        <h2>Pills</h2>
-        <Nav variant="pills">
-          <NavItem>
-            <NavLink active href="#/Labs/Lab2/Active">Active</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#/Labs/Lab2/Link1">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#/Labs/Lab2/Link2" disabled>Disabled</NavLink>
-          </NavItem>
-        </Nav>
-      </div>
-      
-      {/* NAV 3: Cards - REQUIRED */}
-      <div id="wd-css-navigating-with-cards" className="mt-4">
-        <h2>Cards</h2>
-        <Card style={{ width: "18rem" }}>
-          <CardImg variant="top" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
-          <CardBody>
-            <CardTitle>Stacking Starship</CardTitle>
-            <CardText>
-              Stacking the most powerful rocket in history. Mars or bust!
-            </CardText>
-            <Button variant="primary">Boldly Go</Button>
-          </CardBody>
-        </Card>
-      </div>
-    </>
-  );
+    return (
+        <>
+            {/* Tabs - Page 65 */}
+            <div id="wd-css-navigating-with-tabs">
+                <h2>Tabs</h2>
+                <Nav variant="tabs">
+                    <NavItem>
+                        <NavLink href="#" active>Active</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Link</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#" disabled>Disabled</NavLink>
+                    </NavItem>
+                </Nav>
+            </div>
+
+            {/* Pills - Page 65 */}
+            <div id="wd-css-navigating-with-pills" className="mt-3">
+                <h2>Pills</h2>
+                <Nav variant="pills">
+                    <NavItem>
+                        <NavLink active href="#">Active</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Link</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#" disabled>Disabled</NavLink>
+                    </NavItem>
+                </Nav>
+            </div>
+
+            {/* Cards - Page 66 - EXACTLY as textbook */}
+            <div id="wd-css-navigating-with-cards" className="mt-4">
+                <h2>Cards</h2>
+                <Card style={{ width: "18rem" }}>
+                    <CardImg variant="top" src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+                    <CardBody>
+                        <CardTitle>Stacking Starship</CardTitle>
+                        <CardText>
+                            Stacking the most powerful rocket in history. Mars or bust!
+                        </CardText>
+                        <Button variant="primary">Boldly Go</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        </>
+    );
 }
